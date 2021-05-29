538c9fc4d67e75bf3bec67b2b96223df09a13779
marmooo <marmooo@users.noreply.github.com>
2021-05-30 08:34:41 +0900

    bootstrap5

--- src/sw.js
+++ src/sw.js
@@ -5,7 +5,7 @@ var urlsToCache = [
   '/simple-QR/generate/',
   '/simple-QR/index.js',
   '/simple-QR/jsQR.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/encoding-japanese/1.0.30/encoding.min.js',
 ];
 
