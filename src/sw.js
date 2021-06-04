d305f79646defebe733c451f29d0da183417fb7f
marmooo <marmooo@users.noreply.github.com>
2021-06-04 09:41:17 +0900

    update libraries

--- src/sw.js
+++ src/sw.js
@@ -5,7 +5,7 @@ var urlsToCache = [
   '/simple-QR/generate/',
   '/simple-QR/index.js',
   '/simple-QR/jsQR.min.js',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/encoding-japanese/1.0.30/encoding.min.js',
 ];
 
