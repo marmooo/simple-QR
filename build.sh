6649bbe898c8bee69efbd295dbb2ae6ca7aa8d2d
marmooo <marmooo@users.noreply.github.com>
2021-07-15 10:52:54 +0900

    cleanup

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
-dir=docs
-cp -r src/* $dir
-minify src/index.html > $dir/index.html
-minify src/generate/index.html > $dir/generate/index.html
-minify src/scan/index.html > $dir/scan/index.html
-minify src/index.js > $dir/index.js
-minify src/sw.js > $dir/sw.js
+mkdir -p docs
+cp -r src/* docs
+minify -r src -o docs
 
