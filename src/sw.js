bf30a8a6d4f3a8f6ddd7cf657ed436f81849e25f
marmooo <marmooo@users.noreply.github.com>
2021-08-08 16:04:32 +0900

    bump bootstrap from 5.0.2 to 5.1.0

--- src/sw.js
+++ src/sw.js
@@ -6,7 +6,7 @@ var urlsToCache = [
   '/simple-QR/favicon/favicon-48x48.png',
   '/simple-QR/index.js',
   '/simple-QR/jsQR.min.js',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/encoding-japanese/1.0.30/encoding.min.js',
 ];
 
