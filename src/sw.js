40e7abc8e6ca63fbaaeaba149b196f8add86d750
marmooo <marmooo@users.noreply.github.com>
2021-10-17 13:57:03 +0900

    bump bootstrap from 5.1.2 to 5.1.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-10-15 20:33';
+var CACHE_NAME = '2021-10-17 13:42';
 var urlsToCache = [
   "/simple-QR/",
   "/simple-QR/scan/",
@@ -7,7 +7,7 @@ var urlsToCache = [
   "/simple-QR/generate.js",
   "/simple-QR/favicon/favicon-48x48.png",
   "/simple-QR/jsQR.min.js",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/encoding-japanese@1.0.30/encoding.min.js",
 ];
 
