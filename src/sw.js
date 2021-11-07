19ebfc3316bb8fcd197ac96eef9e6cc53201b07d
marmooo <marmooo@users.noreply.github.com>
2021-11-07 15:05:39 +0900

    update jsQR

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-10-17 13:42';
+var CACHE_NAME = '2021-11-07 15:00';
 var urlsToCache = [
   "/simple-QR/",
   "/simple-QR/scan/",
@@ -6,6 +6,7 @@ var urlsToCache = [
   "/simple-QR/generate/",
   "/simple-QR/generate.js",
   "/simple-QR/favicon/favicon-48x48.png",
+  // https://github.com/cozmo/jsQR/tree/8e6a036beafa7053dd44b1b76ac578d22b1b3311
   "/simple-QR/jsQR.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/encoding-japanese@1.0.30/encoding.min.js",
