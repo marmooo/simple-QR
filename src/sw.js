e529bbf8aaeb7ac4cf6c41c9fbc26a4426d175a9
marmooo <marmooo@users.noreply.github.com>
2022-03-08 23:49:37 +0900

    fix path

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-12-09 11:10';
+var CACHE_NAME = '2022-03-08 23:50';
 var urlsToCache = [
   "/simple-QR/",
   "/simple-QR/scan/",
@@ -6,7 +6,6 @@ var urlsToCache = [
   "/simple-QR/generate/",
   "/simple-QR/generate.js",
   "/simple-QR/favicon/favicon-48x48.png",
-  // https://github.com/cozmo/jsQR/tree/8e6a036beafa7053dd44b1b76ac578d22b1b3311
   "/simple-QR/jsQR.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/encoding-japanese@1.0.30/encoding.min.js",
